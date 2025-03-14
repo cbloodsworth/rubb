ae3f3559d39a890776b991dc0d12bc7760c1322d
Chris Bloodsworth <christopherbloodsworth@gmail.com>
2025-03-14 18:09:51 -0400

    Add TokenKind and Token to lexer.
    I also added a Display impl for TokenKind.
    
    Rust has a concept of "trait"s, which you can
    kind of think of like interfaces that we can
    tack onto existing structs.
    
    The "Display" trait allows you to print the
    struct, and provides a `.to_string()` method.
    
    This is helpful for TokenKinds -- when we lex
    into Tokens we may do something like:
    
    ```rs
    if (/* matches left curly bracket, for instance */) {
        let kind = TokenKind::LeftCurly;
    
        return Token {
            kind: kind,
            lexeme: kind.to_string(),
        }
    }
    ```
    
    Note that for STRINGs, NUMBERs, and IDENTIFIERs, we can't
    know the string representation of their kind ahead of time,
    so we are using "STRING", "NUMBER", and "IDENTIFIER"
    respectively. For all other tokens, though, this works as a
    shortcut.

--- src/lexer.rs
+++ src/lexer.rs
@@ -2,5 +2,80 @@ pub fn lex() {
     todo!()
 }
 
+/// Struct representing the tokens in our language.
+struct Token {
+    kind: TokenKind,
+    lexeme: String,
+}
+
+/// Enum representing the kinds of tokens in our language.
+enum TokenKind {
+    // Single-character tokens
+    LeftParen, RightParen,       // (  )
+    LeftBrace, RightBrace,       // [  ]
+    LeftCurly, RightCurly,       // {  }
+    Plus, Minus, Star, Slash,    // +  -  *  /
+    Comma, Dot, Semicolon,       // ,  .  ;
+
+    // One or two character tokens
+    Bang, BangEqual,             // !  !=
+    Equal, EqualEqual,           // =  ==
+    Greater, GreaterEqual,       // >  >=
+    Less, LessEqual,             // <  <=
+    // Literals
+    Identifier, String, Number,
+
+    // Keywords
+    If, Else,                    // if    else
+    For, While,                  // for   while
+    True, False,                 // true  false
+    Func, Return,                // fn    return
+    Let, Const,                  // let   const
+}
+
+/// Allows us to print TokenKinds or convert them to a string.
+impl std::fmt::Display for TokenKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            TokenKind::LeftParen => "(",
+            TokenKind::RightParen => ")",
+            TokenKind::LeftBrace => "[",
+            TokenKind::RightBrace => "]",
+            TokenKind::LeftCurly => "{",
+            TokenKind::RightCurly => "}",
+            TokenKind::Plus => "+",
+            TokenKind::Minus => "-",
+            TokenKind::Star => "*",
+            TokenKind::Slash => "/",
+            TokenKind::Comma => ",",
+            TokenKind::Dot => ".",
+            TokenKind::Semicolon => ";",
+            TokenKind::Bang => "!",
+            TokenKind::BangEqual => "!=",
+            TokenKind::Equal => "=",
+            TokenKind::EqualEqual => "==",
+            TokenKind::Greater => ">",
+            TokenKind::GreaterEqual => ">=",
+            TokenKind::Less => "<",
+            TokenKind::LessEqual => "<=",
+            TokenKind::Identifier => "IDENTIFIER",
+            TokenKind::String => "STRING",
+            TokenKind::Number => "NUMBER",
+            TokenKind::If => "if",
+            TokenKind::Else => "else",
+            TokenKind::For => "for",
+            TokenKind::While => "while",
+            TokenKind::True => "true",
+            TokenKind::False => "false",
+            TokenKind::Func => "fn",
+            TokenKind::Return => "return",
+            TokenKind::Let => "let",
+            TokenKind::Const => "const",
+        };
+
+        write!(f, "{s}")
+    }
+}
+
 #[cfg(test)]
 mod tests;
